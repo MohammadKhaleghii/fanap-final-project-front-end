1ed8f639dd4aea74adecf8b14285d883daf30a79
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2024-03-20 23:06:03 +0330

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
 
 const config: Config = {
   content: [
@@ -8,10 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#56B280",
+        secondary: "black",
+        tertiary: "white",
+      },
+      container: {
+        padding: {
+          sm: "8px",
+          lg: "16px",
+        },
       },
     },
   },
