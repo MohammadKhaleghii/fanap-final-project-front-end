70d54b5c35f894be047ae66b14db0d2f52a04aea
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2024-03-21 00:31:18 +0330

    style: add fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type {AppProps} from "next/app";
+import "../../public/fonts/awesome/css/all.css";
+import "../../public/fonts/iran-sans/fonts.css";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({Component, pageProps}: AppProps) {
   return <Component {...pageProps} />;
 }
