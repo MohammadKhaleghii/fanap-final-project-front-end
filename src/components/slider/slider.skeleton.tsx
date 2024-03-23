3bf11ad10131bc9b2ec4bf9593f84343ea5a57ad
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2024-03-23 15:31:20 +0330

    chore: add slider  skeleton

---
+++ src/components/slider/slider.skeleton.tsx
@@ -0,0 +1,9 @@
+import Skeleton from "react-loading-skeleton";
+
+const SliderSkeleton = () => {
+  return (
+    <Skeleton className="lg:h-[300px] h-[200px] w-full rounded-lg"></Skeleton>
+  );
+};
+
+export default SliderSkeleton;
