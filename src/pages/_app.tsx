f68a415c4c7e92e73e19e55057b32b5bff527d8c
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2024-03-23 15:05:26 +0330

    style: add react loading style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import type {AppProps} from "next/app";
+import "react-loading-skeleton/dist/skeleton.css";
 import "../../public/fonts/awesome/css/all.css";
 import "../../public/fonts/iran-sans/fonts.css";
 
