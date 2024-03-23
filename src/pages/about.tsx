0b570627e8670f5b8b80cf079fda864808109033
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2024-03-23 15:03:36 +0330

    feat: initial about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,10 @@
+import PublicLayout from "@/layouts/public/public-layout";
+
+const AboutPage = () => {
+  return (
+    <PublicLayout>
+      <section className="centered-container">a </section>
+    </PublicLayout>
+  );
+};
+export default AboutPage;
