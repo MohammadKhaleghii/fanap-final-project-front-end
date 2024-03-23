01f70942c3812eb59722450a3d63b90e9b46d23f
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2024-03-23 15:03:24 +0330

    feat: initial contact page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,10 @@
+import PublicLayout from "@/layouts/public/public-layout";
+
+const ContactPage = () => {
+  return (
+    <PublicLayout>
+      <section className="centered-container"> </section>
+    </PublicLayout>
+  );
+};
+export default ContactPage;
