f5ac0d9a9593e4a74c709c1ead48341370700a1a
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2024-03-23 15:32:08 +0330

    fix: assign key for iterate items

--- src/components/slider/index.tsx
+++ src/components/slider/index.tsx
@@ -2,14 +2,14 @@ import {FC} from "react";
 import "swiper/css";
 import {Pagination} from "swiper/modules";
 import {Swiper, SwiperSlide} from "swiper/react";
-import {HomeSliderProps} from "./slider.interface";
+import {SliderProps} from "./slider.interface";
 
-const Slider: FC<HomeSliderProps> = ({slides}) => {
+const Slider: FC<SliderProps> = ({slides}) => {
   return (
     <Swiper pagination modules={[Pagination]} className="mySwiper">
       {slides.length > 0 &&
         slides.map((item) => (
-          <SwiperSlide>
+          <SwiperSlide key={item.id} className="min-h-[300px]">
             <img className="rounded-lg" src={item.imagePath} alt={item.title} />
           </SwiperSlide>
         ))}
