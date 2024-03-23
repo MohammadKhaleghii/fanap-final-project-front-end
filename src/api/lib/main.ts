a9f4e6e3c81aca4d2b3c9c35e70a5d77e8125351
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2024-03-23 16:24:06 +0330

    chore: add apis

---
+++ src/api/lib/main.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import {SearchProductsOutput} from "../dto/search-output";
+
+export const getSliders = () =>
+  axios.get("https://fanap-final-project-json-server.vercel.app/sliders");
+
+export const getArticles = () =>
+  axios.get("https://fanap-final-project-json-server.vercel.app/articles");
+
+export const getCategories = () =>
+  axios.get("https://fanap-final-project-json-server.vercel.app/categories");
+
+export const getTopProducts = () =>
+  axios.get("https://fanap-final-project-json-server.vercel.app/topProducts");
+
+export const getTestimonial = () =>
+  axios.get("https://fanap-final-project-json-server.vercel.app/testimonial");
+
+export const getSearchProducts = (params: SearchProductsOutput) =>
+  axios.get(
+    "https://fanap-final-project-json-server.vercel.app/searchProducts",
+    {
+      params,
+    }
+  );
