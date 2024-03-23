ffd24e369f5a5e04411882f16a78d1b232832278
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2024-03-23 15:02:59 +0330

    feat: initial product details page

---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,10 @@
+import PublicLayout from "@/layouts/public/public-layout";
+
+const ProductDetailsPage = () => {
+  return (
+    <PublicLayout>
+      <section className="centered-container">a </section>
+    </PublicLayout>
+  );
+};
+export default ProductDetailsPage;
