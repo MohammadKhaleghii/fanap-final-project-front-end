9dc43a47c5a7b20eafe269512839eb409f0a9052
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2024-03-23 22:12:26 +0330

    fix: remove inputSize in input component

--- src/components/common/input/index.tsx
+++ src/components/common/input/index.tsx
@@ -3,36 +3,32 @@ import {InputProps} from "./input.interface";
 import {cva} from "class-variance-authority";
 import {cn} from "@/utilities/tailwind-merge";
 
-const inputVariant = cva("px-2 py-3 rounded-sm h-10  text-black bg-white", {
-  variants: {
+const inputVariant = cva(
+  "px-2 py-3 rounded-sm h-10 w-full text-black bg-white",
+  {
     variants: {
-      primary: "border border-primary outline-none",
-      secondary:
-        "border border-gray outline-primary placeholder:text-[#616161]",
+      variants: {
+        primary: "border border-primary outline-none",
+        secondary:
+          "border border-gray outline-primary placeholder:text-[#616161]",
+      },
     },
-    inputSize: {
-      small: "w-[140px]",
-      medium: "w-[216px]",
-      large: "w-[440px]",
+    defaultVariants: {
+      variants: "secondary",
     },
-  },
-  defaultVariants: {
-    variants: "secondary",
-    inputSize: "large",
-  },
-});
+  }
+);
 
 const Input: FC<InputProps> = ({
   variants,
   className,
   children,
-  inputSize,
   ...inputProps
 }) => {
   return (
     <>
       <input
-        className={cn(inputVariant({variants, inputSize, className}))}
+        className={cn(inputVariant({variants, className}))}
         {...inputProps}
       />
       {children}
