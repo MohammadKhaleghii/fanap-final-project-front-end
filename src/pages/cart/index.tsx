8cdd5a726b9ffe5cd5b8ddfb3cab958cec989afd
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2024-03-25 14:26:31 +0330

    chore: initial cart page

---
+++ src/pages/cart/index.tsx
@@ -0,0 +1,10 @@
+import PublicLayout from "@/layouts/public/public-layout";
+
+const CartPage = () => {
+  return (
+    <PublicLayout>
+      <section className="centered-container">This is a cart page</section>
+    </PublicLayout>
+  );
+};
+export default CartPage;
