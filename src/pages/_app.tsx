2d545de1c5763fed5da1dba44bd9b4721b964343
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2024-03-29 16:54:58 +0330

    chore: warp cart provider to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
+import CartProvider from "@/context/cart-provider";
 import "@/styles/globals.css";
 import type {AppProps} from "next/app";
+import {Toaster} from "react-hot-toast";
 import "react-loading-skeleton/dist/skeleton.css";
 import "../../public/fonts/awesome/css/all.css";
 import "../../public/fonts/iran-sans/fonts.css";
-import {Toaster} from "react-hot-toast";
 
 export default function App({Component, pageProps}: AppProps) {
   return (
     <>
-      <Component {...pageProps} />;
+      <CartProvider>
+        <Component {...pageProps} />;
+      </CartProvider>
       <Toaster />
     </>
   );
