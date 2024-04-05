b94afa20354b4d4140515875ae4ba4c5280337d7
MohammadKhaleghii <mhmdkhaleghie@gmail.com>
2024-04-05 16:28:41 +0330

    fix cursor issue in category card and slider component

--- src/components/slider/index.tsx
+++ src/components/slider/index.tsx
@@ -5,13 +5,19 @@ import "swiper/css/pagination";
 import {Pagination} from "swiper/modules";
 import {Swiper, SwiperSlide} from "swiper/react";
 import {SliderProps} from "./slider.interface";
+import {useRouter} from "next/router";
 
 const Slider: FC<SliderProps> = ({slides}) => {
+  const router = useRouter();
   return (
     <Swiper pagination modules={[Pagination]} className="mySwiper">
       {Slider.length > 0 &&
         slides.map((item) => (
-          <SwiperSlide key={item.desktop.id} className="lg:min-h-[300px]">
+          <SwiperSlide
+            key={item.desktop.id}
+            className="lg:min-h-[300px] cursor-pointer "
+            onClick={() => router.push("/search")}
+          >
             <picture>
               <source
                 srcSet={item.mobile.imagePath}
